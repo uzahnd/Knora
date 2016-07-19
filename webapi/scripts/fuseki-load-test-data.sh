4c11357168b1f388fb95033c89d4e48a24ce6b45
ElaineBlath <nataliakorchagina@dhcp-wlan-uzh-89-206-67-126.uzh.ch>
2016-07-19 16:32:11 +0200

    ssrq ontology update

--- webapi/scripts/fuseki-load-test-data.sh
+++ webapi/scripts/fuseki-load-test-data.sh
@@ -4,5 +4,5 @@ curl -X POST -H "Content-type:application/x-www-form-urlencoded" --data-urlencod
 curl -F filedata=@../../knora-ontologies/knora-base.ttl http://localhost:3030/knora-test/data?graph=http://www.knora.org/ontology/knora-base > /dev/null
 curl -F filedata=@../../knora-ontologies/knora-dc.ttl http://localhost:3030/knora-test/data?graph=http://www.knora.org/ontology/dc > /dev/null
 curl -F filedata=@../../knora-ontologies/salsah-gui.ttl http://localhost:3030/knora-test/data?graph=http://www.knora.org/ontology/salsah-gui > /dev/null
-curl -F filedata=@../_test_data/ontologies/ssrq_onto.ttl http://localhost:3030/knora-test/data?graph=http://www.knora.org/ontology/ssrq > /dev/null
-curl -F filedata=@../_test_data/all_data/knora_ssrq_dump2.ttl http://localhost:3030/knora-test/data?graph=http://www.knora.org/data/ssrq > /dev/null
+curl -F filedata=@../_test_data/ontologies/ssrq_onto_share.ttl http://localhost:3030/knora-test/data?graph=http://www.knora.org/ontology/ssrq > /dev/null
+curl -F filedata=@../_test_data/all_data/ssrq_data.ttl http://localhost:3030/knora-test/data?graph=http://www.knora.org/data/ssrq > /dev/null
